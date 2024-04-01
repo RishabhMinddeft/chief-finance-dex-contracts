5dd3dfcbc1f1695578ecde9ace327b679de19a74
MD-YashShah1923 <yash.shah9@minddeft.net>
2024-04-01 12:03:30 +0530

    Changes in code

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,77 @@
 // const { version } = require("hardhat");
 
 require("@nomicfoundation/hardhat-toolbox");
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
-  solidity:{compilers:[{version:"0.5.0"},{version:"0.5.16"},{version:"0.6.0"},{version:"0.6.2"},{version:"0.6.6"},{version:"0.8.24"}]}
+  
+  solidity: {
+    compilers: [{ version: "0.5.0",settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    } }, { version: "0.5.16",settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    } }, { version: "0.6.0",settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    } }, { version: "0.6.2",settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    } }, { version: "0.6.6",settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    } }, { version: "0.8.24",settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    } }],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    }
+  },
+  allowUnlimitedContractSize: true,
+  etherscan: {
+    "apiKey":
+      "BD5NNA18K4YEI2RIR2JSKM6GXDFWDAFGXF"
+
+  },
+  networks: {
+    bsc: {
+      url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+      accounts: ["2305481b90c24d22ff932a5f79668844a44e10b5ab2ad38e3393aa594dca4448"],
+      gas: 4000000000000000,
+      gasPrice: 35000000000,
+      saveDeployments: true,
+
+
+    },
+    mumbai : {
+      url:"https://polygon-mumbai.g.alchemy.com/v2/hML_HRNy_LEP9hIU3JkDgpnw0eVcM6hV",
+      accounts:["2305481b90c24d22ff932a5f79668844a44e10b5ab2ad38e3393aa594dca4448"],
+      gas : 400000000000000
+    
+      
+    }
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
 };
--- ignition/modules/Lock.js
+++ ignition/modules/Lock.js
@@ -1,14 +1,13 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI = 1_000_000_000n;
 
-module.exports = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
+module.exports = buildModule("LocModule", (m) => {
+  const address1 = m.getParameter("address1","0x5969Ad5Abb6D9f1A0336579AD094828d4c3D3140");
+  
+
+  const lock = m.contract("UniswapV2Factory", [address1,address1], {
+    
   });
 
   return { lock };
